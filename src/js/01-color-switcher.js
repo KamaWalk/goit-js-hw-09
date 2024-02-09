46337339439ed5228a5d022934a2ff50acf49ea4
KamaWalk <nierudylis@gmail.com>
2024-02-09 20:27:22 +0100

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let timerChangeColor = null;
 
+startButton.disabled = false;
+stopButton.disabled = true;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    document.body.style.backgroundColor = getRandomHexColor();
+    timerChangeColor = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    });
+
+stopButton.addEventListener('click', () => {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(timerChangeColor);
+});
